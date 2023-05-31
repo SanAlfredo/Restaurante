70dbcb23e62547164cc7c1d111a207dbb905340c
SanAlfredo <alfredo.2009.8@gmail.com>
2023-05-31 13:24:33 -0400

    nueva coneccion a base de datos local mysql, eliminada coneccion a sqlite

--- app/src/main/java/com/example/restaurante/MainActivity.java
+++ app/src/main/java/com/example/restaurante/MainActivity.java
@@ -3,13 +3,22 @@ package com.example.restaurante;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
     //declaración de variables locales
@@ -17,6 +26,9 @@ public class MainActivity extends AppCompatActivity {
     Button btn1, btn2, btn3;
     //funcion de ayuda ventana
     FuncionesHelper funcionesHelper = new FuncionesHelper();
+    //peticiones en internet
+    RequestQueue requestQueue;
+    private static final String tag = "Loggin activity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,59 +64,102 @@ public class MainActivity extends AppCompatActivity {
         btn3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String user=edt1.getText().toString();
-                String pass=edt2.getText().toString();
+                String user = edt1.getText().toString();
+                String pass = edt2.getText().toString();
                 //comprobar campos vacios y logear
                 if (user.isEmpty() || pass.isEmpty()) {
                     funcionesHelper.ventanaMensaje(MainActivity.this, "Debe llenar los campos de: " +
                             "\n\t* Usuario\n\t* Contraseña");
                 } else {
-                    String[] usuario = {edt1.getText().toString()};
-                    buscarUser(usuario, pass);
+                    buscarUser(user, pass);
                 }
             }
         });
     }
 
     //metodo para buscar usuario en la base de datos
-    public void buscarUser(String[] user, String pass1) {
-        //instanciamos data base helper
-        DBHelper helper = new DBHelper(MainActivity.this);
-        //instanciamos la libreria de sqlite
-        SQLiteDatabase db = helper.getReadableDatabase();
-        //generamos la consulta SQL
-        //String consulta = "select * from usuarios where usuario =? and contra =?";
-        String consulta = "select * from usuarios where usuario like?";
-        try {
-            //ejecutamos la consulta dentro un try catch por si hubiera algun error
-            Cursor c = db.rawQuery(consulta, user);
-            if (c != null && c.getCount() > 0) {
-                c.moveToFirst();
-                if (pass1.equals(c.getString(3))){
-                    //definir mensaje para el Toast
-                    String mensaje = "Bienvenido/a: " + c.getString(1);
-                    Toast.makeText(MainActivity.this, mensaje, Toast.LENGTH_LONG).show();
-                    edt1.setText("");
-                    edt2.setText("");
-                    if (c.getInt(4)==1){
-                        Intent intent =new Intent(MainActivity.this,MenuAdmin.class);
-                        startActivity(intent);
-                    }else {
-                        Intent intent =new Intent(MainActivity.this,MenuUser.class);
-                        startActivity(intent);
+    public void buscarUser(String user, String pass1) {
+        //generar la URL que conecta al local host
+        String url = "http:192.168.100.76/ConexionBDRestaurante/buscarUser.php?usuario=" + user + "";
+        StringRequest request = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONObject jsonObject = new JSONObject(response);
+                    String exito = jsonObject.getString("exito");
+                    JSONArray jsonArray = jsonObject.getJSONArray("datos");
+                    if (exito.equals("1")) {
+                        for (int i = 0; i < jsonArray.length(); i++) {
+                            JSONObject object = jsonArray.getJSONObject(i);
+                            String id = object.getString("id");
+                            String nombre = object.getString("nombre");
+                            String usuario = object.getString("usuario");
+                            String contra = object.getString("contra");
+                            int tipo = object.getInt("tipo");
+//                            String tipo2= Integer.toString(tipo);
+//                            String mensaje = "bienvenido " + id + " nombre " + nombre + " usuario " + usuario + " contra " + contra + " tipo " + tipo2;
+//                            Toast.makeText(MainActivity.this, mensaje, Toast.LENGTH_SHORT).show();
+                            if (pass1.equals(contra)) {
+                                //definir mensaje para el Toast
+                                String mensaje = "Bienvenido/a: " + nombre;
+                                Toast.makeText(MainActivity.this, mensaje, Toast.LENGTH_LONG).show();
+                                edt1.setText("");
+                                edt2.setText("");
+                                if (tipo == 1) {
+                                    Intent intent = new Intent(MainActivity.this, MenuAdmin.class);
+                                    startActivity(intent);
+                                } else {
+                                    Intent intent = new Intent(MainActivity.this, MenuUser.class);
+                                    startActivity(intent);
+                                }
+                            } else {
+                                //definir mensaje para la ventana
+                                //String mensaje = "Contraseña incorrecta "+pass1+" base de datos "+c.getString(3);
+                                String mensaje = "Contraseña incorrecta";
+                                funcionesHelper.ventanaMensaje(MainActivity.this, mensaje);
+                            }
+                        }
+                    } else {
+                        funcionesHelper.ventanaMensaje(MainActivity.this, "El usuario y/o contraseña son incorrectas");
                     }
-                }else{
-                    //definir mensaje para la ventana
-                    //String mensaje = "Contraseña incorrecta "+pass1+" base de datos "+c.getString(3);
-                    String mensaje = "Contraseña incorrecta";
-                    funcionesHelper.ventanaMensaje(MainActivity.this, mensaje);
+                } catch (JSONException e) {
+                    e.printStackTrace();
                 }
-            } else {
-                funcionesHelper.ventanaMensaje(MainActivity.this, "El usuario y/o contraseña son incorrectas");
             }
-        } catch (Exception e) {
-            Toast.makeText(getApplication(), "ERROR " + e,
-                    Toast.LENGTH_SHORT).show();
-        }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(MainActivity.this, error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+        requestQueue = Volley.newRequestQueue(MainActivity.this);
+        requestQueue.add(request);
+
+//        //generar un vector json de la consulta
+//        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(URL, response -> {
+//            JSONObject jsonObject = null;
+//            Toast.makeText(MainActivity.this,"aqui llega 2 "+user+" y "+pass1,Toast.LENGTH_SHORT).show();
+//            int i;
+//            for (i = 0; i < response.length(); i++) {
+//                //Log.d(tag, "estoy aqui");
+//                try {
+//                    //ejecutamos la consulta dentro un try catch por si hubiera algun error
+//                    jsonObject = response.getJSONObject(i);
+//                    String pass2 = jsonObject.getString("contra");
+//
+//                } catch (Exception e) {
+//                    Toast.makeText(getApplicationContext(), "ERROR " + e.getMessage(),
+//                            Toast.LENGTH_SHORT).show();
+//                }
+//            }
+//        }, new Response.ErrorListener() {
+//            @Override
+//            public void onErrorResponse(VolleyError error) {
+//                Toast.makeText(getApplicationContext(), "ERROR de conexion",
+//                        Toast.LENGTH_SHORT).show();
+//            }
+//        });
+//        requestQueue = Volley.newRequestQueue(MainActivity.this);
+//        requestQueue.add(jsonArrayRequest);
     }
 }
