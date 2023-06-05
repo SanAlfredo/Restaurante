9e2383677dc83d16548c331f090647f0bf1193ac
SanAlfredo <alfredo.2009.8@gmail.com>
2023-06-05 08:55:20 -0400

    pedidos realizados y guardados en la base de datos con exito

--- app/src/main/java/com/example/restaurante/MenuUser.java
+++ app/src/main/java/com/example/restaurante/MenuUser.java
@@ -6,10 +6,9 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ProgressDialog;
-import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.text.InputType;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -19,6 +18,7 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -31,20 +31,30 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class MenuUser extends AppCompatActivity {
     //declaracion de variables locales
-    Button btn1, btn2, btn3, btn4, btn5, btn6;
+    Button btn1, btn2, btn3, btn4, btn5, btn6, btn8;
     ListView list1;
-    ArrayList<String> listado;
+
     //ventana
     FuncionesHelper funcionesHelper = new FuncionesHelper();
     //peticiones en internet
-    private RequestQueue requestQueue;
+    RequestQueue requestQueue;
     //variable de tipo array List
     private ArrayList<String> datos = new ArrayList<String>();
     private ArrayList<String> datos1 = new ArrayList<String>();
-    //private int c;
+    //array list para enviar los datos de este activity al de pago
+    String[] enviar1 = new String[100];
+    String[] enviar2 = new String[100];
+    int id_venta = 0;
+    ArrayList<String> listado;
+    int cod_cliente = 3;
+    SendDeviseDetails sendDeviseDetails = new SendDeviseDetails();
+    int n = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,6 +67,7 @@ public class MenuUser extends AppCompatActivity {
         btn4 = findViewById(R.id.btn_user_comp);
         btn5 = findViewById(R.id.btn_user_sand);
         btn6 = findViewById(R.id.btn_user_postre);
+        btn8 = findViewById(R.id.btn_user_comprar);
         list1 = findViewById(R.id.list_user_pedir);
 
         //boton Combos
@@ -67,11 +78,13 @@ public class MenuUser extends AppCompatActivity {
                 parseCombo(new Callback() {
                     @Override
                     public void onSuccess(ArrayList<String> datos, ArrayList<String> datos1) {
+                        //si la funcion es llamada correctamente se llama a cargar la Lista
                         cargarLista(datos, datos1);
                     }
 
                     @Override
                     public void onFail(String msg) {
+                        //si falla la funcion en devolver datos
                         funcionesHelper.ventanaMensaje(MenuUser.this, "No hay datos que mostrar");
                     }
                 });
@@ -111,6 +124,7 @@ public class MenuUser extends AppCompatActivity {
                 });
             }
         });
+        //boton de complementos
         btn4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -127,6 +141,7 @@ public class MenuUser extends AppCompatActivity {
                 });
             }
         });
+        //boton para sandwiches
         btn5.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -143,6 +158,14 @@ public class MenuUser extends AppCompatActivity {
                 });
             }
         });
+        //boton para las compras
+        btn8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                regVenta();
+            }
+        });
+
     }
 
     //funcion parseCombo que permite llamarse a si misma
@@ -150,6 +173,7 @@ public class MenuUser extends AppCompatActivity {
         String busq = "combo";
         //generar la URL que conecta al local host
         String url = "http:" + ip + "/ConexionBDRestaurante/buscarProductoNombre.php?nombre=" + busq + "";
+        //funcion que se conecta a la api de respuesta
         StringRequest request = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
@@ -158,6 +182,7 @@ public class MenuUser extends AppCompatActivity {
                     JSONObject jsonObject = new JSONObject(response);
                     String exito = jsonObject.getString("exito");
                     JSONArray jsonArray = jsonObject.getJSONArray("datos");
+                    //limpiar las listas de datos
                     datos.clear();
                     datos1.clear();
                     //si exito es 1
@@ -181,8 +206,10 @@ public class MenuUser extends AppCompatActivity {
                             datos.add(linea);
                             datos1.add(linea1);
                         }
+                        //llamar a la funcion callback para guardar los cambios en los datos
                         onCallback.onSuccess(datos, datos1);
                     } else {
+                        //en caso de no tener resultados
                         funcionesHelper.ventanaMensaje(MenuUser.this, "No se encontraron combos");
                     }
                 } catch (JSONException e) {
@@ -192,9 +219,11 @@ public class MenuUser extends AppCompatActivity {
         }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
+                //en caso de tener un error al leer los datos en la base de datos
                 Toast.makeText(MenuUser.this, error.getMessage(), Toast.LENGTH_SHORT).show();
             }
         });
+        //llamar a la libreria Volley
         requestQueue = Volley.newRequestQueue(MenuUser.this);
         requestQueue.add(request);
     }
@@ -261,40 +290,165 @@ public class MenuUser extends AppCompatActivity {
 
     //funcion encargada de llenar el listView
     public void cargarLista(ArrayList<String> datos, ArrayList<String> datos1) {
+        //limpiar los datos que se envian
+//        enviar1.clear();
+//        enviar2.clear();
         //obtiene los datos y los pone en el array adapter
         listado = datos1;
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(MenuUser.this,
                 android.R.layout.simple_list_item_1, listado);
         //adaptar los datos al List View
         list1.setAdapter(adapter);
+        //cuando se da click a algun item de la lista se activa lo siguiente
         list1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                //se crea un Alert Dialog
                 AlertDialog.Builder builder = new AlertDialog.Builder(MenuUser.this);
+                //en base a un layout propio product_dialog
                 View mView = getLayoutInflater().inflate(R.layout.product_dialog, null);
+                // se instancia las varibles dentro del layout, referenciado a esta vista
                 final TextView tv1 = (TextView) mView.findViewById(R.id.tv_alert_detalle);
                 final EditText edt1 = (EditText) mView.findViewById(R.id.et_alert_cantidad);
                 Button btn7 = (Button) mView.findViewById(R.id.btn_alert_comprar);
+                //se llena el TextView con los datos seleccionados
                 tv1.setText(datos1.get(i));
+                //se separa el string para obtener los datos de codigo, precio del seleccionado
                 String[] separado = datos.get(i).split(":");
                 Float precio = Float.parseFloat(separado[5].trim());
                 String[] separar1 = separado[1].split("\n");
                 int codigo = Integer.parseInt(separar1[0].trim());
+                String producto = separar1[1].trim();
+                //enlazamos el alert dialog con nuestro diseño
+                builder.setView(mView);
+                AlertDialog dialog = builder.create();
+                //este boton del alert dialog
                 btn7.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
                         String c = edt1.getText().toString();
                         if (c.isEmpty()) {
-                            Toast.makeText(MenuUser.this, "Debe poner una cantidad "+precio, Toast.LENGTH_SHORT).show();
+                            Toast.makeText(MenuUser.this, "Debe poner una cantidad ", Toast.LENGTH_SHORT).show();
                         } else {
-                            Toast.makeText(MenuUser.this, "Cantidad: " + c, Toast.LENGTH_SHORT).show();
+                            Float precio_tot = Float.parseFloat(c) * precio;
+                            String envia1 = "Producto: " + producto + "\nCantidad: " + c + "\nPrecio Unidad: " + precio +
+                                    "\nPrecio total: " + precio_tot;
+                            String envia2 = "::" + codigo + "::" + c + "::" + precio_tot;
+                            enviar1[n] = envia1;
+                            enviar2[n] = envia2;
+                            n = n + 1;
+                            Toast.makeText(MenuUser.this, "guardado el pedido", Toast.LENGTH_SHORT).show();
+                            dialog.dismiss();
                         }
+
                     }
                 });
-                builder.setView(mView);
-                AlertDialog dialog = builder.create();
+                //finalmente mostramos el dialog
                 dialog.show();
             }
         });
     }
+
+    //funcion para registrar
+    public void regVenta() {
+        //generar la URL que conecta al local host
+        String url = "http:" + ip + "/ConexionBDRestaurante/regVenta.php?codigo=" + cod_cliente + "";
+        final ProgressDialog progressDialog = new ProgressDialog(MenuUser.this);
+        progressDialog.setMessage("Cargando su pedido...");
+        if (vectorVacio(enviar2) == false) {
+            Toast.makeText(MenuUser.this, "esta vacio", Toast.LENGTH_SHORT).show();
+        } else {
+            progressDialog.show();
+            StringRequest request = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    try {
+                        JSONObject jsonObject = new JSONObject(response);
+                        String exito = jsonObject.getString("exito");
+                        JSONArray jsonArray = jsonObject.getJSONArray("datos");
+                        //si es igual a 1
+                        if (exito.equals("1")) {
+                            //obtener id resultado
+                            JSONObject object = jsonArray.getJSONObject(0);
+                            //obtener los valores
+                            id_venta = Integer.parseInt(object.getString("id"));
+                            progressDialog.dismiss();
+                            progressDialog.show();
+                            for (int i = 0; i < enviar2.length; i++) {
+                                if (enviar2[i] != null) {
+                                    Toast.makeText(MenuUser.this, "paso1", Toast.LENGTH_SHORT).show();
+                                    String[] separado = enviar2[i].split("::");
+                                    int cod = Integer.parseInt(separado[1].trim());
+                                    int can = Integer.parseInt(separado[2].trim());
+                                    Float pre = Float.parseFloat(separado[3].trim());
+                                    try {
+                                        //Toast.makeText(MenuUser.this, "paso 2 "+separado[3], Toast.LENGTH_SHORT).show();
+                                        String url1 = "http:" + ip + "/ConexionBDRestaurante/regPedido.php?codigo1=" + cod +
+                                                "&codigo2=" + id_venta + "&cantidad=" + can + "&precio=" + pre + "";
+                                        StringRequest request1 = new StringRequest(Request.Method.POST, url1, new Response.Listener<String>() {
+                                            @Override
+                                            public void onResponse(String response) {
+//
+                                            }
+                                        }, new Response.ErrorListener() {
+                                            @Override
+                                            public void onErrorResponse(VolleyError error) {
+                                                progressDialog.dismiss();
+                                                Toast.makeText(MenuUser.this, error.getMessage(), Toast.LENGTH_SHORT).show();
+
+                                            }
+                                        });
+                                        requestQueue = Volley.newRequestQueue(MenuUser.this);
+                                        requestQueue.add(request1);
+                                    } catch (Exception e) {
+                                        progressDialog.dismiss();
+                                        e.printStackTrace();
+                                    }
+                                }
+                            }
+                            progressDialog.dismiss();
+                            //aqui ir a la otra ventana
+                            Intent intent = new Intent(MenuUser.this, FacturaPago.class);
+                            startActivity(intent);
+
+                        } else {
+                            progressDialog.dismiss();
+                            funcionesHelper.ventanaMensaje(MenuUser.this, "no se ingreso");
+
+                        }
+                    } catch (JSONException e) {
+                        progressDialog.dismiss();
+                        e.printStackTrace();
+                    }
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    progressDialog.dismiss();
+                    Toast.makeText(MenuUser.this, error.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            });
+            requestQueue = Volley.newRequestQueue(MenuUser.this);
+            requestQueue.add(request);
+        }
+//        if (id_venta != 0) {
+//
+//        }
+
+    }
+
+    public boolean vectorVacio(String[] vector) {
+        boolean respuesta = false;
+        for (int i = 0; i < vector.length; i++) {
+            if (vector[i] != null) {
+                respuesta = true;
+                break;
+            } else {
+                respuesta = false;
+            }
+        }
+        return respuesta;
+    }
+    //Intent intent = new Intent(MenuUser.this, FacturaPago.class);
+    //intent.putExtra("usuario")
 }
